961d9c4df7e66b133c26f75d74d1cbb0d9831ffd
Alexey Vasilyev <bestspajic@gmail.com>
2020-07-25 13:25:06 +0300

    Implement improved version of newton-square-root good_enough

---
+++ exercises/002-newton-square-root-2.exs
@@ -0,0 +1,24 @@
+# Реализация good_enough? из примера 001 не очень эффективна для больших и малых чисел
+# Лучше заканчивать алгоритм, когда guess пактичечки перестал меняться
+
+defmodule Ex do
+  def sqrt(x) do
+    sqrt_iter(x, 1, 0)
+  end
+
+  def sqrt_iter(x, guess, prev_guess) do
+    if guess_stopped_to_change?(guess, prev_guess) do
+      guess
+    else
+      sqrt_iter(x, improve(guess, x), guess)
+    end
+  end
+
+  def guess_stopped_to_change?(guess, prev_guess) do
+    abs(guess - prev_guess) < 0.0000001
+  end
+
+  def improve(guess, x) do
+    (guess + x/guess) / 2
+  end
+end
