55be20a44b5f35f1d79a531104757d879f29b717
Alexey Vasilyev <bestspajic@gmail.com>
2020-07-25 13:57:22 +0300

    Add note on private functions and use them

--- exercises/003-newton-qubic-root.exs
+++ exercises/003-newton-qubic-root.exs
@@ -7,7 +7,7 @@ defmodule Ex do
     iter(x, 1, 0)
   end
 
-  def iter(x, guess, prev_guess) do
+  defp iter(x, guess, prev_guess) do
     if guess_stopped_to_change?(guess, prev_guess) do
       guess
     else
@@ -16,11 +16,11 @@ defmodule Ex do
     end
   end
 
-  def guess_stopped_to_change?(guess, prev_guess) do
+  defp guess_stopped_to_change?(guess, prev_guess) do
     abs(guess - prev_guess) < 0.0000001
   end
 
-  def improve(guess, x) do
+  defp improve(guess, x) do
     ((x / (guess*guess) + 2*guess)) / 3
   end
 end
